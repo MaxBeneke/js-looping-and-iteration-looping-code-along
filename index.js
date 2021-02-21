a0393464e76c20c90775ecb3a8eccdcfb76bbbe9
Max Beneke <max.beneke@gmail.com>
2021-02-21 10:57:46 -0500

    commit tree 885dac8dc2a80ff12dd51a53de026158d5d826df

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return array;
+}
+
+function countDown(num) {
+    console.log(num);
+    if (num > 0) {
+        countDown(num - 1);
+    }
+}
